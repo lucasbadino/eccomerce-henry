de82e3b945679ddd6242d86740d6cbb587dc0b2a
lucasbadino <lucas.badino.7@gmail.com>
2024-10-14 16:51:25 -0300

    docker

--- back/dist/modules/users/usersDto/usersDto.d.ts
+++ back/dist/modules/users/usersDto/usersDto.d.ts
@@ -7,7 +7,7 @@ export declare class CreateUserDto {
     country: string;
     city: string;
 }
-declare const UpdateUserData_base: import("@nestjs/common").Type<Partial<Pick<CreateUserDto, "name" | "email" | "password" | "address" | "phone" | "country" | "city">>>;
+declare const UpdateUserData_base: import("@nestjs/common").Type<Partial<Pick<CreateUserDto, "name" | "email" | "password" | "phone" | "country" | "address" | "city">>>;
 export declare class UpdateUserData extends UpdateUserData_base {
     name?: string;
     email?: string;
